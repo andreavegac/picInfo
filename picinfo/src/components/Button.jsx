b6f8adcb118e1229c32dbdefd4516dd5bd8524e0
Andrea Vega <andrea.gjn@gmail.com>
2023-01-10 21:47:45 +0100

    Added custom text in button

--- picinfo/src/components/Button.jsx
+++ picinfo/src/components/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css'
-function Button(){
-    return <button type="button" className={styles.customButton}>Click Me!</button>;
+function Button(props){
+    return <button type="button" className={styles.customButton}>{props.text}</button>;
 }
 
 export default Button;
