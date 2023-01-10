213213b7f99d45a9d8899aca0875f8908c47a1c5
Andrea Vega <andrea.gjn@gmail.com>
2023-01-10 19:46:46 +0100

    Created header

--- picinfo/src/components/Header.jsx
+++ picinfo/src/components/Header.jsx
@@ -1,7 +1,7 @@
-import './Header.css'
+import styles from'./Header.module.css'
 function Header(props){
-    return <header className='headerComponent'>
-        <div class="overlay">
+    return <header className={styles.headerComponent}>
+        <div className={styles.overlay}>
         <h1>PicInfo</h1>
         <h3>Lorem Picsum data information</h3>
         <p>Showing the information of some pics extracted from Lorem Picsum. This is a PoC to defend doing the project with React instead of plain JS.</p>
