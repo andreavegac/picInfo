db3e0643faaa83e7ceb5a18bf545ea5e55e5bab2
Andrea Vega <andrea.gjn@gmail.com>
2023-01-27 15:37:30 +0100

    Add zoom effect on hover

--- picinfo/src/components/PhotoInformation.jsx
+++ picinfo/src/components/PhotoInformation.jsx
@@ -5,7 +5,9 @@ function PhotoInformation(props) {
   return (
     <div className={styles.cardContainer}>
       <Link to={`/detail/` + props.id} className={styles.link}>
-        <img className={styles.photo} src={props.url}></img>
+        <div className={styles.photoContainer}>
+          <img className={styles.photo} src={props.url}></img>
+        </div>
       </Link>
     </div>
   );
